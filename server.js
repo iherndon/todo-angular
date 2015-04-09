00a50b2a3fc6f2c7427d6ecc9b9fa8ff24bd9462
Ivan <ivan.herndon@detroitlabs.com>
2015-04-09 02:54:54 -0400

    changing structure to work with heroku

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
-var express = require('express'),
-    app = express();
+// var express = require('express'),
+//     app = express();
 
-app.use(express.static(__dirname + ''));
+// app.use(express.static(__dirname + ''));
 
-app.listen(8080)
+// app.listen(8080)
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index.html');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
